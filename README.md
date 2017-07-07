1f9ffea98438f2091059de070c558fea3b2e94f3
= <=>
2017-07-07 18:35:36 -0400

    foo

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-* Read csv file using csvFilePath module <br/>
+* Read csv file using csvtojson module <br/>
 * set currentvariable equal to the current row, for each row in the csv file if it does not include a "www" and does not include a "http" and does not include a "#" <br/>
+* while in the foreach loop push currentvariable into all tweets array <br/>
+* turn all tweets array into csv format using array-to-csv module <br/>
+* write csv formatted all tweets array to parsedtweets.csv using file-system module <br/>
--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const csv = require('csvtojson');
-const csvFilePath = 'twitterdata.csv';
+const toCSV = require('array-to-csv');
+const fs = require('file-system');
+
+
+const csvFilePath = 'twitterdata.csv'; //your file
 const c = console.log;
 let tweets = [];
 
 csv().fromFile(csvFilePath).on('csv',(csvRow)=>{ //this func will 
     csvRow.forEach((tweet)=>{
-    	if(! tweet.includes("http") && tweet.includes("www") && !tweet.includes("#") ){
+    	if(! tweet.includes("http") && !tweet.includes("www.") && !tweet.includes("#")){
     		let current_tweet = Array(tweet);
     		tweets.push(current_tweet);
     	}    	
@@ -14,6 +18,12 @@ csv().fromFile(csvFilePath).on('csv',(csvRow)=>{ //this func will
 })
 .on('done',()=>{
     c('end')
-    c(tweets);
+    fs.writeFile('parsedtweets.csv', toCSV(tweets) , (err) => {
+	  if (!err){
+		c('The file has been saved!');
+	  }else{
+	  	c("error : ", err);
+	  }
+	});
 })
  
